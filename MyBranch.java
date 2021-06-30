014285459e5f0c1c9cb0ca8b517ed26cf4f31776
Bolagani Reeshma <bolagani.reeshma@ibm.com>
2021-06-30 13:32:24 +0530

    Created MyBranch.java

---
+++ MyBranch.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    int time = 20;
+    if (time < 15) {
+      System.out.println("Good day Good to see you.");
+    }
+    else {
+      System.out.println("Good evening wonderful day.");
+    }  
+  }
+}
