7b61d4be72a71ab9d8b679a3b5e4c67ec6ae1a43
Bolagani Reeshma <bolagani.reeshma@ibm.com>
2021-06-30 11:21:08 +0530

    create AtoZ.java

---
+++ AtoZ.java
@@ -0,0 +1,17 @@
+public class Xyz{
+	public void demo(){
+		
+
+		System.out.println("Hello");
+	}
+	public void demo1(){
+		System.out.println("hii");
+	}
+	
+	public static void main(String[] args){
+		
+
+		System.out.println("Hello");
+	}
+
+}
